07ff4d51663bc70e46ded9ee7afe92852df2e986
willow-dias <48129627+willowdias@users.noreply.github.com>
2022-05-14 16:17:47 -0400

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,13 @@
+function openesqueceu(){
+    document.getElementById('esqueceu-senha').style.display="flex"
+}
+function closeesqueceu(){
+    document.getElementById('esqueceu-senha').style.display="none"
+}
+
+function abriremalduvida(){
+    document.getElementById('duvida').style.display="flex"
+}
+function closeemalduvida(){
+    document.getElementById('duvida').style.display="none"
+}
